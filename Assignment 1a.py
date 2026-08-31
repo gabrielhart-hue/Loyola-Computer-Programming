2da251eac564e594af98b0a7a8af993446b4384b
Gabriel Hart <gabrielhart@Gabriels-MacBook-Air-3.local>
2026-08-31 09:02:27 -0700

    assignment 1a is done

---
+++ Assignment 1a.py
@@ -0,0 +1,17 @@
+# Hart, Gabriel
+# Computer Programming p4
+# Assingnment 1a
+# August 27 2026
+
+print("Hello World!")
+print("Hello my name is Gabriel!")
+print("I love python!")
+print(16)
+print(5+5)
+print(4*3)
+print(50-40)
+print(100/5)
+print(100//5)
+print(2**4)
+print(6*3-4+8//2)
+# The difference between the two slashes vs. the one slash is that the the two slashes does not include a decimal point while the one slash does include a decimal
