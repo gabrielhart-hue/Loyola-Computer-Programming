346d16760a745baf56646a6baabf9114e04f1665
Gabriel Hart <gabrielhart@Gabriels-MacBook-Air-3.local>
2026-09-03 21:22:00 -0700

    pushing assignment 1b

---
+++ assignment1b.py
@@ -0,0 +1,32 @@
+# Hart, Gabriel
+# Computer Programming, p4
+# Assignment #1b: Homework 2
+# September 1, 2026
+
+fav_hobby = "guitar" 
+print(fav_hobby)
+name = "John"
+print(f"Hello {name}, would you like to learn some python today?")
+name = "Greg"
+print(name.lower())
+print(name.upper())
+print(name.title())
+famous_quote = "The only way to do great work is to love what you do"
+print("Steve Jobs once said, \"The only way to do great work is to love what you do.\"")
+famous_person = "Steve Jobs"
+message = f'{famous_person} once said, "The only way to do great work is to love what you do."'
+print(message)
+name = "\tEric\n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
+filename = "python_notes.txt"
+print(filename.removesuffix(".txt"))
+print(5 + 6)
+print(20 - 9)
+print(11 * 1)
+print(22 / 2)
+favorite_number = 7
+message = f"My favorite number is {favorite_number}."
+print(message)
---
+++ assignment1c.py
@@ -0,0 +1,26 @@
+# Gabriel Hart
+# Assignment 1c
+
+# Part 1
+name_of_object = input("Enter the name of the object: ")
+mass_kg = input("what is the mass of the object in kg? ")
+velocity = input("what is the velocity of the object in m/s")
+
+name_of_object_clean = name_of_object.strip().title()
+mass_kg_num = float(mass_kg)
+valocity_num = float(velocity)
+
+KE_joules = 1/2*mass_kg_num*(velocity_num**2)
+
+KE_calories = KE_joules/4.184
+
+KE_ergs = KE_joules * 10**7
+
+#print("Kinetic Energy Report for: ", name_of_object_clean)
+output_line_one = f"Kinetic Energy Repot for: {name_of_object_clean}"
+print(output_line_one)
+print("------------------------------")
+out_line_three = f"Joules:\t{KE_joule} J"
+#print("Joules:\t", KE_joules "J")
+print(out_line_three)
+output_line_two = f"Calories:\t{}"
